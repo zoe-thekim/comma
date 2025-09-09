a5f8752e9da8b8c9af5312268aa2c5fb0a60dd76
zoe-thekim <107913237+zoe-thekim@users.noreply.github.com>
2025-09-09 21:14:56 +0900

    FIX: fix error

--- back/src/main/java/com/zoe/web/WebApplication.java
+++ back/src/main/java/com/zoe/web/WebApplication.java
@@ -3,7 +3,6 @@ package com.zoe.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WebApplication {
